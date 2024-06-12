5e631b05b4a63d58cb88bdb53e863728c987bb9a
Hari_225 k <kammari.krishna3@gmail.com>
2024-06-12 12:15:28 +0000

    first_commit on sea_level

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,18 +4,39 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df=pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-
+    y=df['CSIRO Adjusted Sea Level']
+    x=df['Year']
+    fig,ax=plt.subplots(figsize=(6,4))
+    ax=plt.scatter(x,y)
+    
+    # plt.xlabel('Year')
+    # plt.ylabel('Sea Level (inches)')
+    
 
     # Create first line of best fit
-
+    reg=linregress(x,y)
+    x_forecast=pd.Series([i for i in range(1880,2051,1)])
+    y_forecast=reg.slope*x_forecast+reg.intercept
+    plt.plot(x_forecast,y_forecast,'r-')
+    # plt.xlabel('Year')
+    # plt.ylabel('Sea Level (inches)')
 
     # Create second line of best fit
+    df_forc=df.loc[df['Year']>=2000]
+    x_new=df_forc['Year']
+    y_new=df_forc['CSIRO Adjusted Sea Level']
 
-
+    new_reg=linregress(x_new,y_new)
+    x_new_forecast=pd.Series([i for i in range(2000,2051,1)])
+    y_new_forecast=new_reg.slope*x_new_forecast+new_reg.intercept
+    plt.plot(x_new_forecast,y_new_forecast,'orange')
     # Add labels and title
+    plt.title('Rise in Sea Level')
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
 
     
     # Save plot and return data for testing (DO NOT MODIFY)
